5d99c3e02f0207de3c431dd2ef2e7709f74d1eb1
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-03-28 14:21:25 +0900

    Create 20291.py

---
+++ Baekjoon/20291.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/20291
+
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+file = {}
+
+for _ in range(N) :
+    _, extension = sys.stdin.readline().strip().split(".")
+    file[extension] = file.get(extension, 0) + 1
+
+for extension in sorted(file):
+    print(extension, file[extension])
