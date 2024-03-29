08d2394eacfa38d9e915c4445e133329a4ce34e4
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-03-29 14:14:34 +0900

    Update 10816.py

--- Baekjoon/10816.py
+++ Baekjoon/10816.py
@@ -29,3 +29,26 @@ def binary(m, _N, start, end):
 
 for m in _M:
   print(binary(m, _N, 0, len(_N)-1), end=' ')
+
+
+
+# import sys
+
+# N = int(sys.stdin.readline().strip())
+# _N = sorted(list(map(int, sys.stdin.readline().strip().split())))
+# M = int(sys.stdin.readline().strip())
+# _M = list(map(int, sys.stdin.readline().strip().split()))
+
+# DICT = {}
+
+# for n in _N:
+#     if n in DICT:
+#         DICT[n] += 1
+#     else:
+#         DICT[n] = 1
+
+# for m in _M:
+#   if m in DICT:
+#     print(DICT[m], end=' ')
+#   else:
+#      print(0, end=' ')
