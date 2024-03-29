de6192b455bf50a59c6799fccd55997abfb9d97f
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-03-29 14:10:00 +0900

    Create 10816.py

---
+++ Baekjoon/10816.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/10816
+
+import sys
+
+N = int(sys.stdin.readline().strip())
+_N = sorted(list(map(int, sys.stdin.readline().strip().split())))
+M = int(sys.stdin.readline().strip())
+_M = list(map(int, sys.stdin.readline().strip().split()))
+
+DICT = {}
+
+for n in _N:
+  if n in DICT:
+    DICT[n] += 1
+  else:
+    DICT[n] = 1
+
+def binary(m, _N, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    if m == _N[mid]:
+        return DICT[m]
+    elif m < _N[mid]:
+        return binary(m, _N, start, mid - 1)
+    else:
+        return binary(m, _N, mid + 1, end)
+
+
+for m in _M:
+  print(binary(m, _N, 0, len(_N)-1), end=' ')
