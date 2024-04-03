dc1b266a2c02ebc542696b7a3a77bdd8e2af916d
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-04-03 23:42:00 +0900

    Create 1021.py

---
+++ Baekjoon/1021.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/1021
+
+from collections import deque
+
+n, m = map(int, input().split())
+# 뽑아내려고 하는 수의 위치
+index = list(map(int, input().split()))
+
+data = deque([i for i in range(1, n+1)])
+
+count = 0
+for num in index:
+    while 1:
+        if data[0] == num:
+            data.popleft()
+            break
+        else:
+            if data.index(num) <= len(data) // 2:
+                data.rotate(-1)
+                count += 1
+            else:
+                data.rotate(1)
+                count += 1
+
+print(count)
