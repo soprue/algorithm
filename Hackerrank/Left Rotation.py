f0e4e308f92ff823d01da7700b887163a9ee9d5e
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-07-04 17:32:59 +0900

    Create Left Rotation.py

---
+++ Hackerrank/Left Rotation.py
@@ -0,0 +1,25 @@
+import os
+from collections import deque
+
+def rotateLeft(d, arr):
+    queue = deque(arr)
+    queue.rotate(-d)
+    
+    return queue
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
