6418500dbb5a2ef7352508ad0799b6a4ba23f1c0
Gahee Kim <62260343+soprue@users.noreply.github.com>
2024-07-22 23:28:31 +0900

    Create 88. Merge Sorted Array.py

---
+++ LeetCode/88. Merge Sorted Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+         del nums1[m:]
+         del nums2[n:]
+         nums1 += nums2
+         nums1.sort()
