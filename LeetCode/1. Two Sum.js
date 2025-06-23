e7adcac31dfc4fb817c52481f2ff92386c40120c
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-06-23 22:52:10 +0900

    Create 1. Two Sum.js

---
+++ LeetCode/1. Two Sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(num, i);
+    }
+};
