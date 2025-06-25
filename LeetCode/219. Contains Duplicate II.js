5743481b2e55ef24cdb4c9a449e101cc157bf7f2
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-06-25 23:16:38 +0900

    Create 219. Contains Duplicate II.js

---
+++ LeetCode/219. Contains Duplicate II.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map(); // 숫자 → 마지막 등장 인덱스
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+            return true;
+        }
+        map.set(nums[i], i); // 항상 최신 인덱스로 업데이트
+    }
+
+    return false;
+};
