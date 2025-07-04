9f3f3b99577452002cb5fdc95ef28f94cb676511
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-07-04 13:43:32 +0900

    Create 71. Simplify Path.js

---
+++ LeetCode/71. Simplify Path.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    const stack = [];
+    const parts = path.split('/');
+
+    for (let part of parts) {
+        if (part === '' || part === '.') {
+            continue;
+        } else if (part === '..') {
+            stack.pop();
+        } else {
+            stack.push(part);
+        }
+    }
+
+    return '/' + stack.join('/');
+};
