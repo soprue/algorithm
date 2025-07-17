f4842534fe64dd92e4fe3c5dff29e8137d0174a0
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-07-17 15:39:59 +0900

    Create 10866.py

---
+++ Baekjoon/10866.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+queue = deque()
+
+for _ in range(n):
+    command = input().strip().split()
+    
+    if command[0] == "push_front":
+        queue.appendleft(command[1])
+    elif command[0] == "push_back":
+        queue.append(command[1])
+    elif command[0] == "pop_front":
+        print(queue.popleft() if queue else -1)
+    elif command[0] == "pop_back":
+        print(queue.pop() if queue else -1)
+    elif command[0] == "size":
+        print(len(queue))
+    elif command[0] == "empty":
+        print(0 if queue else 1)
+    elif command[0] == "front":
+        print(queue[0] if queue else -1)
+    elif command[0] == "back":
+        print(queue[-1] if queue else -1)
