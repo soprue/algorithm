0e410f0b0cf37355293e755f5a48b4027e0fbf80
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-08-11 12:20:41 +0900

    Create 1541.py

---
+++ Baekjoon/1541.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+parts = input().strip().split("-")
+
+result = sum(map(int, parts[0].split("+")))
+
+for part in parts[1:]:
+    result -= sum(map(int, part.split("+")))
+    
+print(result)
