4d5a9a07730a15cdb0e8b21e35b3f71118ca802d
Gahee Kim <62260343+soprue@users.noreply.github.com>
2025-08-15 22:24:50 +0900

    Delete elice directory

--- elice/정리 정돈을 좋아하는 k씨.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-
-for _ in range(m):
-    i, j, k = map(int, input().split())
-    selected = arr[i - 1:j]
-    selected.sort()
-    print(selected[k - 1])
