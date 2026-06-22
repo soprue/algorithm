c8a4ca9970fee2affc261e000c6213fd3d499d44
Gahee Kim <62260343+soprue@users.noreply.github.com>
2026-06-22 21:59:23 +0900

    Update Counting Valleys.py

--- Hackerrank/Counting Valleys.py
+++ Hackerrank/Counting Valleys.py
@@ -1,25 +1,29 @@
 import os
 
 def countingValleys(steps, path):
-    count = 0
-    current = 0
-    
-    for val in path:
-        if val == "U":
-            if current == -1:
-                count += 1
-            current += 1
-        elif val == "D":
-            current -= 1
-    
-    return count
+    level = 0
+    valleys = 0
+
+    for step in path:
+        if step == 'U':
+            level += 1
+        else:
+            level -= 1
+
+        if step == 'U' and level == 0:
+            valleys += 1
+
+    return valleys
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     steps = int(input().strip())
+
     path = input()
 
     result = countingValleys(steps, path)
+
     fptr.write(str(result) + '\n')
+
     fptr.close()
