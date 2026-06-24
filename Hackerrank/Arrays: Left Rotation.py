fe049ef0e6b52d759f85ac15817047b82248655b
Gahee Kim <62260343+soprue@users.noreply.github.com>
2026-06-24 22:03:09 +0900

    Create Arrays: Left Rotation.py

---
+++ Hackerrank/Arrays: Left Rotation.py
@@ -0,0 +1,23 @@
+import os
+
+def rotLeft(a, d):
+    d %= len(a)
+    return a[d:] + a[:d]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
